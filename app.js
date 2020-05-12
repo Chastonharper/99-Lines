bd5e3dfcdb8f047e3a63bd5d42976eb8d6081c88
Chaston Harper <chastonharper1@gmail.com>
2020-05-12 12:52:16 -0500

    first file

---
+++ app.js
@@ -0,0 +1,13 @@
+let friends = ['Joe', 'Bob', 'Dylan', 'Matt', 'Sam'];
+for(let i=0; i < friends.length; i++) { 
+    console.log(friends[i]+":");
+        for(let n = 99; n >=1; n--) {
+            if (n > 2) {
+                console.log([n] + ' lines of code in the file,' + [n] + ' lines of code;'+ friends[i] + ' strikes one out, clears it all out,' + (n-1) + ' lines of code in the file');
+              }  else if (n == 2) {
+                  console.log ([n] + ' lines of code in the file, ' + [n] + ' lines of code;' + friends[i] + ' strikes one out, clears it all out,' + (n-1) + ' line of code in the file');
+              } else if (n = 1) {
+                  console.log([n] + ' line of code in the file,' + [n] + ' line of code;'+ friends[n] + ' strikes one out, clears it all out, no more lines of code in the file');
+              }
+            }
+        }
